36b6ceb986dcc2cd64a7152f746de40b4b70b2b9
t13ag <tgnuadm@outlook.com>
2025-06-10 20:26:29 +0700

    5_bash_readline_uprobe WIP

---
+++ eunomia/5_bash_readline_uprobe/bash_readline_uprobe.bpf.c
@@ -0,0 +1,40 @@
+//go:build ignore
+#include "../../cilium/vmlinux.h" 
+#include <bpf/bpf_helpers.h>
+#include <bpf/bpf_tracing.h>
+
+#define TASK_COMM_LEN 16
+#define MAX_LINE_SIZE 80
+
+struct event {
+    //u8 , not char , otherwise we'll have problem converting []int8 to []byte in go user program
+    u8 comm[TASK_COMM_LEN];
+    u32 pid;
+    u8 ret[MAX_LINE_SIZE];
+};
+
+struct {
+    __uint(type, BPF_MAP_TYPE_RINGBUF);
+    __uint(max_entries, 1 << 24);
+    __type(value , struct event);
+} events SEC(".maps");
+
+SEC("uretprobe//bin/bash:readline")
+int BPF_KRETPROBE(printret,const void *ret) {
+    struct event *e;
+    if (!ret) {
+        return 0;
+    }
+
+    bpf_get_current_comm(e->comm,sizeof(e->comm));
+    e->pid = bpf_get_current_pid_tgid() >> 32;
+    bpf_probe_read_user_str(e->ret,sizeof(e->ret),ret);
+    e = bpf_ringbuf_reserve(&events,sizeof(e),0);
+    if (!e) {
+        return 0;
+    }
+    bpf_ringbuf_submit(e,0);
+    return 0;
+};
+
+char LICENSE[] SEC("license") = "GPL";
---
+++ eunomia/5_bash_readline_uprobe/main.go
@@ -0,0 +1,79 @@
+//go:build linux
+
+package main
+
+import (
+	"bytes"
+	"encoding/binary"
+	"errors"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/cilium/ebpf/link"
+	"github.com/cilium/ebpf/ringbuf"
+	"github.com/cilium/ebpf/rlimit"
+	"golang.org/x/sys/unix"
+)
+
+//go:generate go run github.com/cilium/ebpf/cmd/bpf2go -tags linux -target amd64 bpf bash_readline_uprobe.bpf.c -- -I../cilium/headers
+
+func main() {
+	stopper := make(chan os.Signal, 1)
+	signal.Notify(stopper, os.Interrupt, syscall.SIGTERM)
+
+	if err := rlimit.RemoveMemlock(); err != nil {
+		log.Fatal(err)
+	}
+
+	objs := bpfObjects{}
+
+	if err := loadBpfObjects(&objs, nil); err != nil {
+		log.Fatalf("loading object %v", err)
+	}
+	defer objs.Close()
+
+	link_exit, err := link.AttachTracing(link.TracingOptions{
+		Program: objs.bpfPrograms.Printret,
+	})
+
+	if err != nil {
+		log.Fatalf("linking object: %v", err)
+	}
+	defer link_exit.Close()
+
+	rd, err := ringbuf.NewReader(objs.bpfMaps.Events)
+	if err != nil {
+		log.Fatalf("opening ringbuf reader %v", err)
+	}
+	defer rd.Close()
+
+	go func() {
+		<-stopper
+		if err := rd.Close(); err != nil {
+			log.Fatalf("closing buffer: %v", err)
+		}
+	}()
+
+	var event bpfEvent
+
+	for {
+		record, err := rd.Read()
+		if err != nil {
+			if errors.Is(err, ringbuf.ErrClosed) {
+				log.Println("receive signal, exiting")
+				return
+			}
+			log.Printf("reading from buffer: %s", err)
+			continue
+		}
+
+		if err := binary.Read(bytes.NewBuffer(record.RawSample), binary.LittleEndian, &event); err != nil {
+			log.Printf("parsing ring buff event %s", err)
+			continue
+		}
+		log.Printf("kretprobe event: PID %d comm %s ret %s", event.Pid, unix.ByteSliceToString(event.Comm[:]), unix.ByteSliceToString(event.Ret[:]))
+
+	}
+}
