0d9a95265487e273600b0c37759bae2aaf8813ce
t13ag <tgnuadm@outlook.com>
2026-03-24 21:56:36 +0700

    handle userspace param -> bpf global const

--- eunomia/9-runqlat/main.go
+++ eunomia/9-runqlat/main.go
@@ -19,13 +19,11 @@ import (
 
 func main() {
 
-	/*
-	filter_cg := flag.Bool("filter_cg",false,"filter the by cgroup")
-	targ_per_process := flag.Bool("targ_per_process",false,"group runqueue latency by process")
-	targ_per_thread := flag.Bool("targ_per_thread",false,"group runqueue latency by thread")
-	targ_per_pidns:= flag.Bool("targ_per_pidns",false,"group runqueue latency by pid namespace")
-	targ_ms := flag.Bool("targ_ms",false,"set latency unit to miliseconds. Default to microseconds")
-	*/
+	// filter_cg_ptr := flag.Bool("filter_cg",false,"filter the by cgroup")
+	targ_per_process_ptr := flag.Bool("targ_per_process",false,"group runqueue latency by process")
+	targ_per_thread_ptr := flag.Bool("targ_per_thread",false,"group runqueue latency by thread")
+	// targ_per_pidns_ptr := flag.Bool("targ_per_pidns",false,"group runqueue latency by pid namespace")
+	// targ_ms_ptr := flag.Bool("targ_ms",false,"set latency unit to miliseconds. Default to microseconds")
 
 	flag.Parse()
 
@@ -38,11 +36,29 @@ func main() {
 
 	objs := bpfObjects{}
 
+	// Load spec manually to edit global constant before LoadAndAssign
+	// If not need to change global constant, just use `loadBpfObjects`
+	// See https://ebpf-go.dev/concepts/global-variables/#static-global-variables
 	spec, err := loadBpf()
 	if err != nil {
 		log.Fatalf("loading spec from binary %v",err)
 	}
 
+	/*
+	for k := range(spec.Variables) {
+		fmt.Printf("spec variable key: %s\n",k)
+	}
+	*/
+	if err := spec.Variables["targ_per_process"].Set(*targ_per_process_ptr); err != nil {
+		log.Fatalf("setting targ_per_process arg :%v",err)
+	}
+
+	if err := spec.Variables["targ_per_thread"].Set(*targ_per_thread_ptr); err != nil {
+		log.Fatalf("setting targ_per_thread arg :%v",err)
+	}
+
+
+	// Load and assign spec to object 
 	if err := spec.LoadAndAssign(&objs, nil); err != nil {
 		log.Fatalf("Load and Assign object %v",err)
 	}
